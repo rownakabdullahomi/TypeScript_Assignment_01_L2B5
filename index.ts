789d1d06ae6169701fc0422bec82ec20e0c345b1
rownakabdullahomi <rownakabdullahomi@gmail.com>
2025-05-08 01:42:46 +0600

    problem 6  solved

--- index.ts
+++ index.ts
@@ -55,3 +55,21 @@ class Car extends Vehicle {
 }
 
 
+interface Product {
+  name: string;
+  price: number;
+}
+
+function getMostExpensiveProduct(products: Product[]): Product | null {
+  if (products.length === 0) {
+    return null;
+  }
+
+  let mostExpensiveProduct = products[0];
+  for (const product of products) {
+    if (product.price > mostExpensiveProduct.price) {
+      mostExpensiveProduct = product;
+    }
+  }
+  return mostExpensiveProduct;
+}
