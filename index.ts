c368b7a2ce37e27283c15161f4986abff8332aae
rownakabdullahomi <rownakabdullahomi@gmail.com>
2025-05-08 17:09:00 +0600

    problem 8  solved

--- index.ts
+++ index.ts
@@ -103,3 +103,17 @@ function getDayType(day: Day): "Weekday" | "Weekend" {
   }
 }
 
+async function squareAsync(n: number): Promise<number> {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (n < 0) {
+        reject(new Error("Negative number not allowed"));
+      } else {
+        resolve(n * n);
+      }
+    }, 1000);
+  });
+}
+
+
+
